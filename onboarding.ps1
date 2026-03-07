78df1b5e0ff9bc36d75abe30392735475a60f5e6
Steven van Rooyen <steve@sslvr.net>
2026-03-07 22:02:08 +1000

    Enhance onboarding process by adding email templates for new users and integrating Outlook draft creation for onboarding emails

--- onboarding.ps1
+++ onboarding.ps1
@@ -127,6 +127,7 @@ Import-Module Microsoft.Graph.Users
 Import-Module Microsoft.Graph.Groups
 Import-Module Microsoft.Graph.Identity.DirectoryManagement
 Import-Module Microsoft.Graph.Teams
+Import-Module Microsoft.Graph.Mail
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
@@ -142,6 +143,10 @@ $Script:Config = [ordered]@{
   # Output directory for per-user onboarding email HTML files
   # Each file can be opened and copy-pasted into an Outlook draft
   OnboardingEmailOutputDir = '.\outputs'
+
+  # Sender details for onboarding emails (change to your own name before running)
+  SenderName  = 'Steve'
+  SenderTitle = 'IT Technical Officer'
 
   # TODO (Roadmap item 7): Structured log file path
   # LogPath = 'C:\Temp\MsaOnboarding.log'
@@ -285,7 +290,7 @@ $Script:Config = [ordered]@{
 
   # Cross-campus Teams chat groups
   TeamsChatCrossCampus = @{
-    SupportWorkers = '19:cross-campus-support-workers-thread-id@thread.v2'  # TODO: get actual thread ID from MSA
+    SupportWorkers = '19:d8d84084e7fc4eea9326d88a2cc8a831@thread.v2'
   }
 
   # Job titles that qualify for Support Worker chat groups
@@ -441,6 +446,9 @@ $Script:Config = [ordered]@{
 # Cached verified domains (populated by Connect-MsaGraph)
 $Script:VerifiedDomains = @()
 
+# Cached current user UPN for Graph calls that need it (e.g. Mail.ReadWrite)
+$Script:CurrentUserUpn = $null
+
 # =========================
 # GRAPH CONNECT
 # =========================
@@ -453,10 +461,17 @@ function Connect-MsaGraph {
     'User.ReadWrite.All',
     'Group.ReadWrite.All',
     'Directory.ReadWrite.All',
-    'Chat.ReadWrite.All'
+    'Chat.ReadWrite.All',
+    'Mail.ReadWrite'
   )
 
   Connect-MgGraph -Scopes $scopes | Out-Null
+
+  # Cache current user UPN for Graph calls (e.g. creating Outlook drafts)
+  $Script:CurrentUserUpn = (Get-MgContext).Account
+  if ([string]::IsNullOrWhiteSpace($Script:CurrentUserUpn)) {
+    Write-Warning "Could not determine current user UPN from Graph context. -AddToDrafts may not work."
+  }
 
   # Cache verified domains once per session to avoid repeated Graph calls
   $Script:VerifiedDomains = @(Get-MgDomain | Where-Object { $_.IsVerified -eq $true } | Select-Object -ExpandProperty Id)
@@ -776,7 +791,8 @@ function Invoke-MsaEntraOnboardingFromCsv {
   [CmdletBinding(SupportsShouldProcess=$true)]
   param(
     [string]$CsvPath = '.\onboarding.csv',
-    [switch]$SkipExisting
+    [switch]$SkipExisting,
+    [switch]$AddToDrafts
   )
 
   if (-not (Test-Path $CsvPath)) { throw "CSV not found: $CsvPath" }
@@ -912,16 +928,68 @@ function Invoke-MsaEntraOnboardingFromCsv {
       })
       $newUsersCreated = $true
 
-      # Generate onboarding email HTML file for manual send
+      # Generate onboarding email HTML file(s) for manual send
       $emailDir = $Script:Config.OnboardingEmailOutputDir
       if (-not (Test-Path $emailDir)) {
         New-Item -Path $emailDir -ItemType Directory -Force | Out-Null
       }
       $safeUpn = $upn -replace '[\\/:*?"<>|]', '_'
+      $senderName  = $Script:Config.SenderName
+      $senderTitle = $Script:Config.SenderTitle
+      $campusState = $c.State.ToString().Trim().ToUpper()
+
+      # Primary onboarding email (MSA or MSV based on state)
+      if ($campusState -eq 'VIC') {
+        $emailSubject = 'MSV IT Onboarding'
+        $emailBody = New-MsvOnboardingEmailBody -FirstName $firstName -Upn $upn -TempPassword $password -SenderName $senderName -SenderTitle $senderTitle
+      } else {
+        $emailSubject = 'MSA IT Onboarding'
+        $emailBody = New-MsaOnboardingEmailBody -FirstName $firstName -Upn $upn -TempPassword $password -SenderName $senderName -SenderTitle $senderTitle
+      }
       $emailPath = Join-Path $emailDir "$safeUpn.html"
-      $emailBody = New-MsaQldOnboardingEmailBody -FirstName $firstName -Upn $upn -TempPassword $password
       $emailBody | Out-File -FilePath $emailPath -Encoding UTF8
       Write-Host "  Email draft saved: $emailPath" -ForegroundColor DarkGray
+
+      # Add primary onboarding email to Outlook Drafts via Graph
+      if ($AddToDrafts -and -not [string]::IsNullOrWhiteSpace($personalEmail)) {
+        try {
+          $toRecipients = @(@{ EmailAddress = @{ Address = $personalEmail } })
+          $ccRecipients = @(@{ EmailAddress = @{ Address = $upn } })
+          New-MgUserMessage -UserId $Script:CurrentUserUpn `
+            -Subject $emailSubject `
+            -Body @{ ContentType = 'HTML'; Content = $emailBody } `
+            -ToRecipients $toRecipients `
+            -CcRecipients $ccRecipients | Out-Null
+          Write-Host "  Outlook draft created: $emailSubject -> $personalEmail (CC: $upn)" -ForegroundColor DarkGray
+        } catch {
+          Write-Warning "Could not create Outlook draft for $displayName : $($_.Exception.Message)"
+        }
+      }
+
+      # VIC mandatory training email (second email for MSV staff only)
+      if ($campusState -eq 'VIC') {
+        $trainingSubject = 'Mandatory Training - MSV Staff'
+        $trainingBody = New-MsvMandatoryTrainingEmailBody -FirstName $firstName -SenderName $senderName -SenderTitle $senderTitle
+        $trainingPath = Join-Path $emailDir "$safeUpn-mandatory-training.html"
+        $trainingBody | Out-File -FilePath $trainingPath -Encoding UTF8
+        Write-Host "  VIC training email draft saved: $trainingPath" -ForegroundColor DarkGray
+
+        # Add VIC training email to Outlook Drafts via Graph
+        if ($AddToDrafts -and -not [string]::IsNullOrWhiteSpace($personalEmail)) {
+          try {
+            $toRecipients = @(@{ EmailAddress = @{ Address = $personalEmail } })
+            $ccRecipients = @(@{ EmailAddress = @{ Address = $upn } })
+            New-MgUserMessage -UserId $Script:CurrentUserUpn `
+              -Subject $trainingSubject `
+              -Body @{ ContentType = 'HTML'; Content = $trainingBody } `
+              -ToRecipients $toRecipients `
+              -CcRecipients $ccRecipients | Out-Null
+            Write-Host "  Outlook draft created: $trainingSubject -> $personalEmail (CC: $upn)" -ForegroundColor DarkGray
+          } catch {
+            Write-Warning "Could not create VIC training Outlook draft for $displayName : $($_.Exception.Message)"
+          }
+        }
+      }
 
       Write-Host "Created: $displayName ($upn)" -ForegroundColor Green
 
@@ -995,15 +1063,17 @@ function Invoke-MsaEntraOnboardingFromCsv {
 }
 
 # =========================
-# EMAIL TEMPLATE (QLD MVP)
+# EMAIL TEMPLATES
 # =========================
 
-function New-MsaQldOnboardingEmailBody {
+function New-MsaOnboardingEmailBody {
   [CmdletBinding()]
   param(
     [Parameter(Mandatory)][string]$FirstName,
     [Parameter(Mandatory)][string]$Upn,
-    [Parameter(Mandatory)][string]$TempPassword
+    [Parameter(Mandatory)][string]$TempPassword,
+    [Parameter(Mandatory)][string]$SenderName,
+    [Parameter(Mandatory)][string]$SenderTitle
   )
 
 @"
@@ -1012,7 +1082,7 @@ function New-MsaQldOnboardingEmailBody {
 
 <p>Hi $FirstName,</p>
 
-<p><strong>Welcome to the MSA Team!</strong> My name is Steve. I am an IT Technical Officer servicing all MSA schools. There are just a few more things needed to get you started with us.</p>
+<p><strong>Welcome to the MSA Team!</strong> My name is $SenderName. I am an $SenderTitle servicing all MSA schools. There are just a few more things needed to get you started with us.</p>
 
 <p><strong>Please see below your MSA email account which is accessible via Microsoft:</strong></p>
 
@@ -1035,19 +1105,142 @@ function New-MsaQldOnboardingEmailBody {
 
 <p><strong>Connecting with MSA</strong></p>
 
-<p>With this onboarding email comes access to our IT Help Platform, hosted by Freshworks, that will give you all the basics of connecting to our wireless network, printers and other services. Firstly, you'll need to follow the guide to sign into the Freshworks system utilising your Microsoft account details. Once logged in, you'll be able to see our primary portal offering you various services such as lodging help tickets for support, browsing our support articles or making requests for computers or tools.</p>
+<p>With this onboarding email comes access to our IT Help Platform, hosted by Freshworks, that will give you all the basics of connecting to our wireless network, printers and other services. Firstly, you'll need to <a href="https://service.msa.qld.edu.au/support/solutions/articles/75000121803">follow this guide</a> to sign into the Freshworks system utilising your Microsoft account details. Once logged in, you'll be able to see our primary portal offering you various services such as lodging help tickets for support, browsing our support articles or making requests for computers or tools. We highly recommend perusing the following articles to ensure you can access systems as expected on your first day.</p>
 
-<p>Our recommended reading articles are:</p>
+<p>Our recommended reading can all <a href="https://service.msa.qld.edu.au/support/solutions/75000028330">be found at this link</a>, but our recommended reading articles are:</p>
 
 <ul>
-<li>How to Connect to MSA WiFi Networks</li>
-<li>Logging into MSA Computers</li>
-<li>Logging IT Service Tickets</li>
-<li>How to Login to Compass</li>
-<li>How to Get Microsoft Office</li>
-<li>MSA Email Groups</li>
-<li>Phone Line Extensions</li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000118671">How to Connect to MSA WiFi Networks</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000111898">Logging into MSA Computers</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000092029">Logging IT Service Tickets</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000121602">How to Login to Compass</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000082216">How to Get Microsoft Office</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000108846">MSA Email Groups</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000120006">Phone Line Extensions</a></li>
 </ul>
+
+<p>If you have any questions, please do not hesitate to email <a href="mailto:ITService@msa.qld.edu.au">ITService@msa.qld.edu.au</a> to submit a ticket and receive support from our team.</p>
+
+</body>
+</html>
+"@
+}
+
+function New-MsvOnboardingEmailBody {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory)][string]$FirstName,
+    [Parameter(Mandatory)][string]$Upn,
+    [Parameter(Mandatory)][string]$TempPassword,
+    [Parameter(Mandatory)][string]$SenderName,
+    [Parameter(Mandatory)][string]$SenderTitle
+  )
+
+@"
+<html>
+<body style="font-family: Calibri, Arial, sans-serif; font-size: 11pt;">
+
+<p>Hi $FirstName,</p>
+
+<p><strong>Welcome to the MSV Team!</strong> My name is $SenderName. I am an $SenderTitle servicing all MSV schools. There are just a few more things needed to get you started with us.</p>
+
+<p><strong>Please see below your MSV email account which is accessible via Microsoft:</strong></p>
+
+<p>
+<a href="https://office.com/">Microsoft Account Access (https://office.com/)</a><br/>
+<br/>
+<strong>Username:</strong> $Upn<br/>
+<strong>Password:</strong> $TempPassword
+</p>
+
+<p>If you have any issues accessing your MSV email account, please let me know. Any future emails from IT or the school will come to your MSV email.</p>
+
+<hr/>
+
+<p><strong>Cyber Security Training</strong></p>
+
+<p>On your first day, you will be issued Cyber Security training as part of your employment at MSV. This training is compulsory for all staff and must be completed within the allocated time frame of three weeks. New training programs are issued three times per term and this will be for the entirety of your employment. Ensuring staff are equipped with the skills necessary to identify criminal cyber activity is paramount to the protection of MSV.</p>
+
+<hr/>
+
+<p><strong>Connecting with MSV</strong></p>
+
+<p>With this onboarding email comes access to our IT Help Platform, hosted by Freshworks, that will give you all the basics of connecting to our wireless network, printers and other services. Firstly, you'll need to <a href="https://service.msa.qld.edu.au/support/solutions/articles/75000121803">follow this guide</a> to sign into the Freshworks system utilising your Microsoft account details. Once logged in, you'll be able to see our primary portal offering you various services such as lodging help tickets for support, browsing our support articles or making requests for computers or tools. We highly recommend perusing the following articles to ensure you can access systems as expected on your first day.</p>
+
+<p>Our recommended reading can all <a href="https://service.msa.qld.edu.au/support/solutions/75000028330">be found at this link</a>, but our recommended reading articles are:</p>
+
+<ul>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000118671">How to Connect to MSV WiFi Networks</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000111898">Logging into MSV Computers</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000092029">Logging IT Service Tickets</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000121602">How to Login to Compass</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000082216">How to Get Microsoft Office</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000108846">MSV Email Groups</a></li>
+<li><a href="https://service.msa.qld.edu.au/support/solutions/articles/75000120006">Phone Line Extensions</a></li>
+</ul>
+
+<p>If you have any questions, please do not hesitate to email <a href="mailto:ITService@msa.qld.edu.au">ITService@msa.qld.edu.au</a> to submit a ticket and receive support from our team.</p>
+
+</body>
+</html>
+"@
+}
+
+function New-MsvMandatoryTrainingEmailBody {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory)][string]$FirstName,
+    [Parameter(Mandatory)][string]$SenderName,
+    [Parameter(Mandatory)][string]$SenderTitle
+  )
+
+@"
+<html>
+<body style="font-family: Calibri, Arial, sans-serif; font-size: 11pt;">
+
+<p>Hi $FirstName,</p>
+
+<p>As part of your onboarding with MSV, you are required to complete the following mandatory training modules. Please complete both courses and send your certificates as instructed below.</p>
+
+<hr/>
+
+<p><strong>Child Protection</strong> (approx. 30 minutes to complete)</p>
+
+<p>Please note, I would recommend you read school policies on child protection/mandatory reporting so you can breeze through the course.</p>
+
+<ol>
+<li>Click on this link to access the online training portal for Non-Government schools: <a href="https://training.infosharing.vic.gov.au">https://training.infosharing.vic.gov.au</a></li>
+<li>Create your account by completing all sections displayed
+  <ul>
+  <li>In the <strong>Role</strong> section, Assistant Teachers and Support Workers should select <em>Teacher's Aid and Education Support</em>. Everyone else is obvious.</li>
+  <li>In the <strong>About Your Organisation</strong> section, under Service Type select <em>Independent School</em> and under Organisation Type select <em>not-for-profit organisation</em>.</li>
+  <li>We don't have a Service Number so you can skip this section.</li>
+  </ul>
+</li>
+<li>Confirm your email address</li>
+<li>Agree to the terms and policies</li>
+<li>Select the <strong>Protecting Children</strong> tile and select <em>Protecting Children - Mandatory Reporting and Other Obligations Non-Government Schools</em> course</li>
+<li>On the information page, under <strong>Learning Module</strong> click the hyperlink to <em>Mandatory Reporting and Other Obligations</em> and this will take you to the e-learning training</li>
+<li>Complete all sections and quizzes within the module. Please note, you must click on everything within each module for it to register as complete. Skipping through doesn't work.</li>
+<li>You'll then have an assessment to complete when you return to the information page under <strong>Assessment</strong></li>
+<li>Once complete, follow the step that will take you back to the course information page where you can download a copy of your certificate</li>
+<li>Save as PDF, <strong>change file name to &lt;FULL NAME Protecting Children Certificate 2025&gt;</strong></li>
+<li>Send this to <a href="mailto:bundoora@msv.vic.edu.au">bundoora@msv.vic.edu.au</a> for record keeping</li>
+</ol>
+
+<hr/>
+
+<p><strong>Information Sharing for Education Workforces</strong> (approx. 30 minutes to complete)</p>
+
+<ol>
+<li>Click on this link to access the online training portal for Non-Government schools: <a href="https://training.infosharing.vic.gov.au">https://training.infosharing.vic.gov.au</a></li>
+<li>Select the <strong>Education Workforces</strong> tile then select the <em>Information Sharing for Education Workforces</em> course</li>
+<li>On the information page, under the <strong>eLearn Modules</strong> section, there will be <strong>4 modules</strong> for you to complete. Click on Module 1 to start and follow the instructions within. Please note, you must click on everything within each module for it to register as complete. Skipping through doesn't work.</li>
+<li>Please complete all 4 modules. There are mini quizzes within each but no end of course assessment.</li>
+<li>Once complete, follow the step that will take you back to the course information page where you can download a copy of your certificate</li>
+<li>Save as PDF, <strong>change file name to &lt;FULL NAME Information Sharing Certificate 2025&gt;</strong></li>
+<li>Send this to <a href="mailto:bundoora@msv.vic.edu.au">bundoora@msv.vic.edu.au</a> for record keeping</li>
+</ol>
 
 <p>If you have any questions, please do not hesitate to email <a href="mailto:ITService@msa.qld.edu.au">ITService@msa.qld.edu.au</a> to submit a ticket and receive support from our team.</p>
 
@@ -1062,7 +1255,7 @@ function New-MsaQldOnboardingEmailBody {
 
 <#
 0) Prerequisites (once per machine):
-   Install-Module Microsoft.Graph.Authentication, Microsoft.Graph.Users, Microsoft.Graph.Groups, Microsoft.Graph.Identity.DirectoryManagement -Scope CurrentUser
+   Install-Module Microsoft.Graph.Authentication, Microsoft.Graph.Users, Microsoft.Graph.Groups, Microsoft.Graph.Identity.DirectoryManagement, Microsoft.Graph.Teams, Microsoft.Graph.Mail -Scope CurrentUser
    
 1) Dot-source to load functions into session:
    . .\onboarding.ps1
@@ -1073,6 +1266,9 @@ function New-MsaQldOnboardingEmailBody {
 3) Dry run:
    Invoke-MsaEntraOnboardingFromCsv -SkipExisting -WhatIf
 
-4) Real run:
+4) Real run (HTML files only):
    Invoke-MsaEntraOnboardingFromCsv -SkipExisting
+
+5) Real run (HTML files + Outlook drafts via Graph):
+   Invoke-MsaEntraOnboardingFromCsv -SkipExisting -AddToDrafts
 #>
